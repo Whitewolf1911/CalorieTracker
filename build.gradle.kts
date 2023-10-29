2a92b8d5c628e74727692335405b28f47cbd96cb
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-10-29 15:53:46 +0300

    create modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
 
         // NOTE: Do not place your application dependencies here; they belong
@@ -16,4 +16,3 @@ buildscript {
 tasks.register("clean", Delete::class){
     delete(rootProject.buildDir)
 }
-
