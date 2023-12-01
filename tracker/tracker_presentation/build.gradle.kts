ff520e10708fac6e58d4bf1f192d4d78ba986755
Ali İhsan Başoğlu <ali.basoglu@wunder.is>
2023-12-01 17:07:11 +0300

    implement tracker overview headers

--- tracker/tracker_presentation/build.gradle.kts
+++ tracker/tracker_presentation/build.gradle.kts
@@ -11,6 +11,8 @@ android {
 
 dependencies {
     implementation(project(Modules.core))
+    implementation(project(Modules.coreUi))
+    
     implementation(project(Modules.trackerDomain))
 
     implementation(Coil.coilCompose)
